Added scaling and fixed some bugs
In this version, thrust is implemented but not working. Fixed the limit of control to be from -1 to 1 instead of 0 to 1. Added mass scaling.

ascent_problem.py
@@ -6,27 +6,63 @@ from numpy import linalg
 from openmdao.api import Problem, IndepVarComp, ExecComp, ScipyOptimizer, pyOptSparseDriver
 from ozone.api import ODEIntegrator
 from ascent_function import AscentFunction
-from orbital_condition_one import OrbitalConditionOne
-from orbital_condition_two import OrbitalConditionTwo
-from orbital_condition_three import OrbitalConditionThree
-from thrust_vector_constraint import ThrustVectorConstraint
+# from orbital_condition_one import OrbitalConditionOne
+# from orbital_condition_two import OrbitalConditionTwo
+# from orbital_condition_three import OrbitalConditionThree
+# from thrust_vector_constraint import ThrustVectorConstraint
+from constraint_group import OrbitalConditionOne, OrbitalConditionTwo, OrbitalConditionThree, ThrustVectorConstraint
+# from mass_constraint import MassConstraint ###
 # from lsdo_viz.api import Problem
 # from matplotlib.patches import FancyArrowPatch
 # from mpl_toolkits.mplot3d.proj3d import proj_transform
 # from mpl_toolkits.mplot3d.axes3d import Axes3D
 
 num = 51 # number of nodes
-
+mscale = 15103. #11517.828034303415 #15103. #11414.
 # ---------------------------------------------------------------------------------------------------------
 # integrator group
 
-ode_function = AscentFunction(system_init_kwargs={'g0': 1.625,'R0':1738100.,'vex':3047.80,'w2':1.,'T':45000.})
+ode_function = AscentFunction(system_init_kwargs={'g0': 1.625,'R0':1738100.,'vex':3047.80,'w2':1.,'T':45000.,'m_scale':mscale})
 formulation = 'solver-based'
-method_name = 'GaussLegendre2'
-# method_name = 'RK4'
+method_name = 'RK4'
 initial_time = 0.
 normalized_times = np.linspace(0., 1., num)
-initial_conditions = {'rx': 1., 'ry': 0., 'rz': 0., 'Vx': 0., 'Vy': 0., 'Vz': 0., 'm': 11414.}
+
+if 1:
+    initial_conditions = {'rx': 1.,
+                          'ry': 0.,
+                          'rz': 0.,
+                          'Vx': 0.,
+                          'Vy': 0.,
+                          'Vz': 0.,
+                          'm': 15103./mscale} #11414.
+
+if 0:
+    initial_conditions = {'rx': 0.217158628100769,
+                          'ry': 0.194469906825522,
+                          'rz':-0.965394998582636 ,
+                          'Vx':-0.705532585273920,
+                          'Vy':-0.633100822123821,
+                          'Vz':-0.342184217384921,
+                          'm': 14674.490379463516/mscale} # Abort Time = 0s
+
+if 0:
+    initial_conditions = {'rx': 0.051473261347652,
+                          'ry': 0.046131470657302,
+                          'rz':-1.004802504773337,
+                          'Vx':-0.432215441070972,
+                          'Vy':-0.386722961483122,
+                          'Vz':-0.070907047408024,
+                          'm': 11517.828034303415/mscale} # Abort Time = 300s
+
+if 0:
+    initial_conditions = {'rx':-0.000005774494439052802,
+                          'ry':-0.000005214669074132337,
+                          'rz':-0.998826773853324,
+                          'Vx':-0.008185309454260,
+                          'Vy':-0.007410961907563,
+                          'Vz':-0.0006580193916236741,
+                          'm': 8268.2787241951082/mscale} # Abort Time = 610s
 
 integrator = ODEIntegrator(ode_function, formulation, method_name,
     initial_time=initial_time, normalized_times=normalized_times,
@@ -39,6 +75,7 @@ constraint_one = OrbitalConditionOne(a=1.04603,ecc=0.0385)
 constraint_two = OrbitalConditionTwo(a=1.04603)
 constraint_three = OrbitalConditionThree(inc=np.pi/2)
 constraint_thrust = ThrustVectorConstraint(num_nodes=num)
+# constraint_mass = MassConstraint(num_nodes=num) ###
 
 # ---------------------------------------------------------------------------------------------------------
 # problem formulation 1 - Using connections
@@ -47,19 +84,17 @@ prob = Problem()
 
 # Define independent variable components
 prob.model.add_subsystem('final_time_comp', IndepVarComp('final_time', val=1.0))
-prob.model.add_subsystem('ux_comp', IndepVarComp('ux', val=0.1, shape=(num,1)))
-prob.model.add_subsystem('uy_comp', IndepVarComp('uy', val=0.1, shape=(num,1)))
-prob.model.add_subsystem('uz_comp', IndepVarComp('uz', val=0.1, shape=(num,1)))
+prob.model.add_subsystem('ux_comp', IndepVarComp('ux', val= 0.1, shape=(num,1)))
+prob.model.add_subsystem('uy_comp', IndepVarComp('uy', val= 0.1, shape=(num,1)))
+prob.model.add_subsystem('uz_comp', IndepVarComp('uz', val= 0.1, shape=(num,1)))
 # prob.model.add_subsystem('T_comp', IndepVarComp('T', shape=(num,1))) # Add thrust as indep. var.
 
 # Add integrator group
 prob.model.add_subsystem('integrator_group', integrator)
 
-# Add constraint groups
-prob.model.add_subsystem('ConstraintOne', constraint_one)
-prob.model.add_subsystem('ConstraintTwo', constraint_two)
-prob.model.add_subsystem('ConstraintThree', constraint_three)
-prob.model.add_subsystem('ConstraintThrust', constraint_thrust)
+# Add objective group
+# prob.model.add_subsystem('mass_objective', ExecComp('m_obj = 1. - m')) ###
+# prob.model.connect('integrator_group.state:m', 'mass_objective.m', src_indices=-1)
 
 # Issue connections of independent components
 prob.model.connect('final_time_comp.final_time', 'integrator_group.final_time')
@@ -68,6 +103,7 @@ prob.model.connect('uy_comp.uy', 'integrator_group.dynamic_parameter:uy')
 prob.model.connect('uz_comp.uz', 'integrator_group.dynamic_parameter:uz')
 
 # Issue connections of constraint 1
+prob.model.add_subsystem('ConstraintOne', constraint_one)
 prob.model.connect('integrator_group.state:rx', 'ConstraintOne.rx', src_indices=-1)
 prob.model.connect('integrator_group.state:ry', 'ConstraintOne.ry', src_indices=-1)
 prob.model.connect('integrator_group.state:rz', 'ConstraintOne.rz', src_indices=-1)
@@ -76,6 +112,7 @@ prob.model.connect('integrator_group.state:Vy', 'ConstraintOne.Vy', src_indices=
 prob.model.connect('integrator_group.state:Vz', 'ConstraintOne.Vz', src_indices=-1)
 
 # Issue connections of constraint 2
+prob.model.add_subsystem('ConstraintTwo', constraint_two)
 prob.model.connect('integrator_group.state:rx', 'ConstraintTwo.rx', src_indices=-1)
 prob.model.connect('integrator_group.state:ry', 'ConstraintTwo.ry', src_indices=-1)
 prob.model.connect('integrator_group.state:rz', 'ConstraintTwo.rz', src_indices=-1)
@@ -84,6 +121,7 @@ prob.model.connect('integrator_group.state:Vy', 'ConstraintTwo.Vy', src_indices=
 prob.model.connect('integrator_group.state:Vz', 'ConstraintTwo.Vz', src_indices=-1)
 
 # Issue connections of constraint 3
+prob.model.add_subsystem('ConstraintThree', constraint_three)
 prob.model.connect('integrator_group.state:rx', 'ConstraintThree.rx', src_indices=-1)
 prob.model.connect('integrator_group.state:ry', 'ConstraintThree.ry', src_indices=-1)
 prob.model.connect('integrator_group.state:rz', 'ConstraintThree.rz', src_indices=-1)
@@ -91,64 +129,34 @@ prob.model.connect('integrator_group.state:Vx', 'ConstraintThree.Vx', src_indice
 prob.model.connect('integrator_group.state:Vy', 'ConstraintThree.Vy', src_indices=-1)
 prob.model.connect('integrator_group.state:Vz', 'ConstraintThree.Vz', src_indices=-1)
 
-# Issue connections of constraint 4
+# Issue connections of thrust constraint
+prob.model.add_subsystem('ConstraintThrust', constraint_thrust)
 prob.model.connect('ux_comp.ux', 'ConstraintThrust.ux')
 prob.model.connect('uy_comp.uy', 'ConstraintThrust.uy')
 prob.model.connect('uz_comp.uz', 'ConstraintThrust.uz')
 
 prob.model.add_design_var('final_time_comp.final_time', lower=1./1034.2)
-prob.model.add_design_var('ux_comp.ux',lower=0.,upper=1.)
-prob.model.add_design_var('uy_comp.uy',lower=0.,upper=1.)
-prob.model.add_design_var('uz_comp.uz',lower=0.,upper=1.)
+prob.model.add_design_var('ux_comp.ux',lower=-1.,upper=1.)
+prob.model.add_design_var('uy_comp.uy',lower=-1.,upper=1.)
+prob.model.add_design_var('uz_comp.uz',lower=-1.,upper=1.)
+
 prob.model.add_objective('final_time_comp.final_time')
+# prob.model.add_objective('mass_objective.m_obj') ###
+
 prob.model.add_constraint('ConstraintOne.C1', equals=0.)
 prob.model.add_constraint('ConstraintTwo.C2', equals=0.)
 prob.model.add_constraint('ConstraintThree.C3', equals=0.)
-prob.model.add_constraint('ConstraintThrust.CT', upper=1.)
-prob.model.add_constraint('integrator_group.state:m', lower=1., indices=[-1])
+prob.model.add_constraint('ConstraintThrust.CT', equals=1.)
 
-# ---------------------------------------------------------------------------------------------------------
-# problem formulation 2 - Using promote
-
-# prob = Problem()
-#
-# # Define independent variable components
-# prob.model.add_subsystem('final_time_comp', IndepVarComp('final_time', val=1.0))
-# prob.model.add_subsystem('ux_comp', IndepVarComp('ux', shape=(num,1)))
-# prob.model.add_subsystem('uy_comp', IndepVarComp('uy', shape=(num,1)))
-# prob.model.add_subsystem('uz_comp', IndepVarComp('uz', shape=(num,1)))
-# # prob.model.add_subsystem('T_comp', IndepVarComp('T', shape=(num,1))) # Add thrust as indep. var.
-#
-# # Add integrator group
-# prob.model.add_subsystem('integrator_group', integrator)
-#
-# # Add constraint groups
-# prob.model.add_subsystem('ConstraintOne', constraint_one, promotes=['rx','ry','rz','Vx','Vy','Vz','C1'])
-# prob.model.add_subsystem('ConstraintTwo', constraint_two, promotes=['rx','ry','rz','Vx','Vy','Vz','C2'])
-# prob.model.add_subsystem('ConstraintThree', constraint_three, promotes=['rx','ry','rz','Vx','Vy','Vz','C3'])
-# prob.model.add_subsystem('ConstraintThrust', constraint_thrust, promotes=['ux','uy','uz',CT'])
-#
-# # Issue connections of independent components
-# prob.model.connect('final_time_comp.final_time', 'integrator_group.final_time')
-# prob.model.connect('ux_comp.ux', 'integrator_group.dynamic_parameter:ux')
-# prob.model.connect('ux_comp.uy', 'integrator_group.dynamic_parameter:uy')
-# prob.model.connect('ux_comp.uz', 'integrator_group.dynamic_parameter:uz')
-#
-# prob.model.add_design_var('final_time_comp.final_time', lower=1./806.)
-# prob.model.add_design_var('ux_comp.ux')
-# prob.model.add_design_var('uy_comp.uy')
-# prob.model.add_design_var('uz_comp.uz')
-# prob.model.add_objective('final_time_comp.final_time')
-# prob.model.add_constraint('ConstraintOne.C1', equals=0.)
-# prob.model.add_constraint('ConstraintOne.C2', equals=0.)
-# prob.model.add_constraint('ConstraintOne.C3', equals=0.)
-# prob.model.add_constraint('ConstraintOne.CT', equals=0.)
+prob.model.add_constraint('integrator_group.state:m', lower=1000./mscale, indices=[-1])
 
 # ---------------------------------------------------------------------------------------------------------
 # problem optimizer
 
 prob.driver = pyOptSparseDriver()
 prob.driver.options['optimizer'] = 'SNOPT'
+# prob.driver.opt_settings['Major feasibility tolerance'] = 2e-7
+# prob.driver.opt_settings['Major optimality tolerance'] = 2e-7
 
 # ---------------------------------------------------------------------------------------------------------
 # problem setup
@@ -159,7 +167,7 @@ if 0:
     prob.check_partials(compact_print=True)
     exit()
 
-prob.run()
+prob.run_driver()
 
 t = prob['integrator_group.times']
 rx = prob['integrator_group.state:rx']
@@ -173,76 +181,70 @@ uy = prob['integrator_group.dynamic_parameter:uy']
 uz = prob['integrator_group.dynamic_parameter:uz']
 m = prob['integrator_group.state:m']
 
-# data = [rx, ry, rz, Vx, Vy, Vz, m, ux, uy, uz, t]
-# print("All data")
-# print(data)
-# pickle.dump(data,open("data_no_om.dat","wb"))
-
 if 1:
     r = np.zeros(num)
     for i in range(0,num):
         r[i] = linalg.norm([rx[i],ry[i],rz[i]],2)
 
-    print(r)
-    plt.plot(1034.2*prob['integrator_group.times'], (1738100.*r - 1738100.))
+    #print(r)
+    plt.plot(1034.2*t, (1738100.*r - 1738100.))
     plt.xlabel('time')
     plt.ylabel('altitude')
     plt.grid()
     plt.show()
-print(' ')
-if 0:
+
+if 1:
     V = np.zeros(num)
     for i in range(0,num):
         V[i] = linalg.norm([Vx[i],Vy[i],Vz[i]],2)
 
-    print(V)
-    plt.plot(1034.2*prob['integrator_group.times'], 1680.6*V)
+    #print(V)
+    plt.plot(1034.2*t, 1680.6*V)
     plt.xlabel('time')
     plt.ylabel('velocity')
     plt.grid()
     plt.show()
-print(' ')
-if 0:
+
+if 1:
     u = np.zeros(num)
     for i in range(0,num):
-        u[i] = linalg.norm([ux[i],uy[i],uz[i]],2)
+        u[i] = ux[i]**2 + uy[i]**2 + uz[i]**2
 
-    print(u)
     plt.plot(1034.2*prob['integrator_group.times'], u)
     plt.xlabel('time')
     plt.ylabel('control')
     plt.ylim(-0.2,1.2)
     plt.grid()
     plt.show()
-print(' ')
-if 0:
-    plt.plot(1034.2*prob['integrator_group.times'], m)
+#print(' ')
+if 1:
+    plt.plot(1034.2*prob['integrator_group.times'], mscale*m)
     plt.xlabel('time')
     plt.ylabel('mass')
     plt.grid()
     plt.show()
-print(' ')
-if 0:
+#print(' ')
+if 1:
     plt.plot(1034.2*prob['integrator_group.times'], ux)
     plt.xlabel('time')
     plt.ylabel('control x')
-    plt.ylim(-0.2,1.2)
+    plt.ylim(-1.2,1.2)
     plt.grid()
     plt.show()
-print(' ')
-if 0:
+#print(' ')
+if 1:
     plt.plot(1034.2*prob['integrator_group.times'], uy)
     plt.xlabel('time')
     plt.ylabel('control y')
-    plt.ylim(-0.2,1.2)
+    plt.ylim(-1.2,1.2)
     plt.grid()
     plt.show()
-print(' ')
-if 0:
+#print(' ')
+if 1:
     plt.plot(1034.2*prob['integrator_group.times'], uz)
     plt.xlabel('time')
     plt.ylabel('control z')
-    plt.ylim(-0.2,1.2)
+    plt.ylim(-1.2,1.2)
     plt.grid()
     plt.show()
 
@@ -318,5 +320,19 @@ if 0:
 # print(np.shape(m))
 # print(np.shape(u))
 # print(np.shape(t))
+
+# data min time
 # data = [r, V, u, t]
-# pickle.dump(data,open("data_no_om.dat","wb"))
+# pickle.dump(data,open("data_mintime.dat","wb"))
+# data = [m]
+# pickle.dump(data,open("data_mintime_mass.dat","wb"))
+# data = [ux, uy, uz]
+# pickle.dump(data,open("data_mintime_control.dat","wb"))
+
+# data max prop
+# data = [r, V, u, t]
+# pickle.dump(data,open("data_maxprop.dat","wb"))
+# data = [m]
+# pickle.dump(data,open("data_maxprop_mass.dat","wb"))
+# data = [ux, uy, uz]
+# pickle.dump(data,open("data_maxprop_control.dat","wb"))

ascent_system.py
@@ -9,6 +9,7 @@ class AscentSystem(ExplicitComponent):
         self.options.declare('vex', default=1., types=(int, float))
         self.options.declare('w2', default=1., types=(int, float))
         self.options.declare('T', default=1., types=(int, float))
+        self.options.declare('m_scale', default=1., types=(int, float))
 
     def setup(self):
         num = self.options['num_nodes']
@@ -17,6 +18,7 @@ class AscentSystem(ExplicitComponent):
         vex = self.options['vex']
         w2 = self.options['w2']
         T = self.options['T']
+        m_scale = self.options['m_scale']
 
         self.add_input('ux', shape=(num,1))
         self.add_input('uy', shape=(num,1))
@@ -37,7 +39,7 @@ class AscentSystem(ExplicitComponent):
         self.add_output('dVz_dt', shape=(num,1))
         self.add_output('dm_dt', shape=(num,1))
 
-        self.declare_partials('*','*', dependent=False)
+        # self.declare_partials('*','*', dependent=False)
 
         self.declare_partials('drx_dt', 'Vx', rows=np.arange(num), cols=np.arange(num))
         self.declare_partials('dry_dt', 'Vy', rows=np.arange(num), cols=np.arange(num))
@@ -51,6 +53,9 @@ class AscentSystem(ExplicitComponent):
         self.declare_partials('dVx_dt', 'm', rows=np.arange(num), cols=np.arange(num))
         self.declare_partials('dVy_dt', 'm', rows=np.arange(num), cols=np.arange(num))
         self.declare_partials('dVz_dt', 'm', rows=np.arange(num), cols=np.arange(num))
+        self.declare_partials('dm_dt', 'ux', rows=np.arange(num), cols=np.arange(num))
+        self.declare_partials('dm_dt', 'uy', rows=np.arange(num), cols=np.arange(num))
+        self.declare_partials('dm_dt', 'uz', rows=np.arange(num), cols=np.arange(num))
 
     def compute(self, inputs, outputs):
         g0 = self.options['g0']
@@ -58,19 +63,25 @@ class AscentSystem(ExplicitComponent):
         vex = self.options['vex']
         w2 = self.options['w2']
         T = self.options['T']
+        m_scale = self.options['m_scale']
 
         outputs['drx_dt'] = inputs['Vx']
         outputs['dry_dt'] = inputs['Vy']
         outputs['drz_dt'] = inputs['Vz']
-        outputs['dVx_dt'] = - w2*inputs['rx'] + (T * inputs['ux']) / (inputs['m'] * g0)
-        outputs['dVy_dt'] = - w2*inputs['ry'] + (T * inputs['uy']) / (inputs['m'] * g0)
-        outputs['dVz_dt'] = - w2*inputs['rz'] + (T * inputs['uz']) / (inputs['m'] * g0)
-        outputs['dm_dt'] = - ((T / vex) * np.sqrt(R0/g0))
+        outputs['dVx_dt'] = - w2*inputs['rx'] + (T * inputs['ux']) / (inputs['m'] * m_scale * g0)
+        outputs['dVy_dt'] = - w2*inputs['ry'] + (T * inputs['uy']) / (inputs['m'] * m_scale * g0)
+        outputs['dVz_dt'] = - w2*inputs['rz'] + (T * inputs['uz']) / (inputs['m'] * m_scale * g0)
+        outputs['dm_dt'] = - (T / (m_scale * vex)) * np.sqrt(R0/g0) * np.sqrt(inputs['ux']**2 + inputs['uy']**2 + inputs['uz']**2)
+
+        # print(inputs['m'])
 
     def compute_partials(self, inputs, partials):
         g0 = self.options['g0']
+        R0 = self.options['R0']
+        vex = self.options['vex']
         T = self.options['T']
         w2 = self.options['w2']
+        m_scale = self.options['m_scale']
 
         ux = inputs['ux'][:, 0]
         uy = inputs['uy'][:, 0]
@@ -84,9 +95,12 @@ class AscentSystem(ExplicitComponent):
         partials['dVx_dt', 'rx'] = - w2
         partials['dVy_dt', 'ry'] = - w2
         partials['dVz_dt', 'rz'] = - w2
-        partials['dVx_dt', 'ux'] = T / (m * g0)
-        partials['dVy_dt', 'uy'] = T / (m * g0)
-        partials['dVz_dt', 'uz'] = T / (m * g0)
-        partials['dVx_dt', 'm'] = - (T * ux) / (m**2 * g0)
-        partials['dVy_dt', 'm'] = - (T * uy) / (m**2 * g0)
-        partials['dVz_dt', 'm'] = - (T * uz) / (m**2 * g0)
+        partials['dVx_dt', 'ux'] = T / (m * m_scale * g0)
+        partials['dVy_dt', 'uy'] = T / (m * m_scale * g0)
+        partials['dVz_dt', 'uz'] = T / (m * m_scale * g0)
+        partials['dVx_dt', 'm'] = - (T * ux) / (m**2 * m_scale * g0)
+        partials['dVy_dt', 'm'] = - (T * uy) / (m**2 * m_scale * g0)
+        partials['dVz_dt', 'm'] = - (T * uz) / (m**2 * m_scale * g0)
+        partials['dm_dt', 'ux'] = - (T / (m_scale * vex)) * np.sqrt(R0/g0) * (ux / np.sqrt(ux**2 + uy**2 + uz**2))
+        partials['dm_dt', 'uy'] = - (T / (m_scale * vex)) * np.sqrt(R0/g0) * (uy / np.sqrt(ux**2 + uy**2 + uz**2))
+        partials['dm_dt', 'uz'] = - (T / (m_scale * vex)) * np.sqrt(R0/g0) * (uz / np.sqrt(ux**2 + uy**2 + uz**2))